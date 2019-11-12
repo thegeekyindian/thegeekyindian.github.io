bc806167d2f7bced41433558ced3954172f71684
Geeky Indian <thegeekyindian84@gmail.com>
2019-11-12 23:52:14 +0530

    Added support for side nav

---
+++ vendor.6760291e9c5519a4e868.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{200:function(n,o,p){p(201),p(0),p(83),n.exports=p(29)}},[[200,0,1]]]);
+//# sourceMappingURL=vendor.6760291e9c5519a4e868.bundle.js.map
--- vendor.b7c979df404f9a6f8209.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{189:function(n,o,p){p(190),p(0),p(80),n.exports=p(40)}},[[189,0,1]]]);
-//# sourceMappingURL=vendor.b7c979df404f9a6f8209.bundle.js.map
