cba46962bce14ade0120bed1b0a378d98da4b444
Geeky Indian <57512694+thegeekyindian@users.noreply.github.com>
2023-03-26 01:48:33 +0530

    Added design pattern changes

--- vendor.58cbbe6854ed993a920a.bundle.js
+++ vendor.58cbbe6854ed993a920a.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{215:function(n,o,p){p(125),p(216),p(0),p(74),n.exports=p(28)}},[[215,0,1]]]);
-//# sourceMappingURL=vendor.a384e98c86aa7b32becd.bundle.js.map
+//# sourceMappingURL=vendor.58cbbe6854ed993a920a.bundle.js.map
