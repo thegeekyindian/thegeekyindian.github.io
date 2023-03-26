966c332564c7cca9d31cc9f3513fc2989668f271
Geeky Indian <57512694+thegeekyindian@users.noreply.github.com>
2023-03-26 12:53:42 +0530

    Removed dummy links of about page
    Removed dummy links of about page

--- vendor.58cbbe6854ed993a920a.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{215:function(n,o,p){p(125),p(216),p(0),p(74),n.exports=p(28)}},[[215,0,1]]]);
-//# sourceMappingURL=vendor.58cbbe6854ed993a920a.bundle.js.map
---
+++ vendor.da7c0e1f912a50c8b5ab.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{212:function(n,o,p){p(124),p(213),p(0),p(73),n.exports=p(28)}},[[212,0,1]]]);
+//# sourceMappingURL=vendor.da7c0e1f912a50c8b5ab.bundle.js.map
